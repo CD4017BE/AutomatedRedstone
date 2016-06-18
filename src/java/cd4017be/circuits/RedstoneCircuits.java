18e92b732f772f0067914fa41bc5a34cdf8c59e8
CD4017BE <cd4017be@gmail.com>
2016-06-18 23:36:13 +0200

    v4.1.0

--- src/java/cd4017be/circuits/RedstoneCircuits.java
+++ src/java/cd4017be/circuits/RedstoneCircuits.java
@@ -34,7 +34,7 @@ import static cd4017be.circuits.Objects.*;
  *
  * @author CD4017BE
  */
-@Mod(modid="Circuits", name="Automated Redstone", version="4.0.0")
+@Mod(modid="Circuits", useMetadata = true)
 public class RedstoneCircuits 
 {
     // The instance of your mod that Forge uses.
